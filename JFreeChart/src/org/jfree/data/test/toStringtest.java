8bfc2b07f7adeb4173fdf34691a68bc7398ccb0d
ArtinRezaee <artin.anzabee@gmail.com>
2018-02-06 16:33:59 -0700

    Contains, Expand, getLowerBount, intersects, and toString jUnit tests done

---
+++ JFreeChart/src/org/jfree/data/test/toStringtest.java
@@ -0,0 +1,41 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class toStringtest{
+
+	private Range exampleRange;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		exampleRange = new Range(2.0, 10.0);
+	}
+	
+	@Test
+	public void toStringTest() {
+		assertEquals("The two stings are not equal", "Range[2.0,10.0]" , this.exampleRange.toString());
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+
+}
