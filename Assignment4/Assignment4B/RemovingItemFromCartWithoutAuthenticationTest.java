9d011d6f531f930f9604bff3ca975b11feb3b538
jenardkin.cabia <jenardkin.cabia@cs.ucalgary.ca>
2018-03-22 10:58:29 -0600

    add/remove items from cart

---
+++ Assignment4/Assignment4B/RemovingItemFromCartWithoutAuthenticationTest.java
@@ -0,0 +1,76 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class RemovingItemFromCartWithoutAuthenticationTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testRemovingItemFromCartWithoutAuthentication() throws Exception {
+    driver.get("https://www.amazon.com/");
+    driver.findElement(By.linkText("Gift Cards")).click();
+    driver.findElement(By.xpath("//img[@alt='Print-at-Home']")).click();
+    driver.findElement(By.id("gc-buy-box-atc")).click();
+    driver.findElement(By.id("hlb-view-cart-announce")).click();
+    driver.findElement(By.xpath("//input[@value='Delete']")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
